35ca1a7f4667aec4f16591b8eccc2a994355e021
hydsoled <dato_apple@yahoo.com>
2020-06-04 23:26:47 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BackgroundComponent} from './background/background.component';
+import {GameControlsComponent} from './background/game-controls/gameControls.component';
+import {TableComponent} from './background/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    GameControlsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/background/game-controls/gameControls.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-game-controls',
+  templateUrl: './gameControls.component.html',
+  styleUrls: ['./gameControls.component.css']
+})
+export class GameControlsComponent {
+
+}
